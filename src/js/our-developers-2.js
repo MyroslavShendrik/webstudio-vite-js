bcdbe8d30fdf6465e4f36c8a9286c34294980734
MyroslavShendrik <citrus.user2204@gmail.com>
2025-11-29 04:25:55 +1400

    Update our-developers-2.js

--- src/js/our-developers-2.js
+++ src/js/our-developers-2.js
@@ -122,19 +122,24 @@ let dataDevelopersList = [
 // Перевірка: чи всі дані готові
 console.log("Масив розробників перед localStorage:", dataDevelopersList);
 
-// Зберігаємо у localStorage
+//! Зберігаємо у localStorage dataDevelopersList
+//? умова: якщо localStarage пустий, то додаємо базовий об'єкт dataDevelopersList
+if(localStorage.getItem("dataDevelopers") === undefined){
 localStorage.setItem("dataDevelopers", JSON.stringify(dataDevelopersList));
+}
+// localStorage.setItem("dataDevelopers", JSON.stringify(dataDevelopersList));
 console.log("Дані збережено у localStorage");
 
-// Беремо з localStorage
-const storedData = localStorage.getItem("dataDevelopers");
-if (!storedData) {
-  console.error("Помилка: localStorage порожній!");
-} else {
-  console.log("Дані з localStorage отримано успішно:", JSON.parse(storedData));
-  dataDevelopersList = JSON.parse(storedData);
-}
-
+//! забираємо дані в dataDevelopersList з localStorage
+// const storedData = localStorage.getItem("dataDevelopers");
+// if (!storedData) {
+//   console.error("Помилка: localStorage порожній!");
+// } else {
+//   console.log("Дані з localStorage отримано успішно:", JSON.parse(storedData));
+//   dataDevelopersList = JSON.parse(storedData);
+// }
+dataDevelopersList = localStorage.getItem("dataDevelopers");
+console.log("dataDevelopersList:",dataDevelopersList);
 // Генеруємо HTML
 const markup = dataDevelopersList
   .map(
