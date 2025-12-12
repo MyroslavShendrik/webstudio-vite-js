a52e4da0047051a6660df602a68619fe083d1df1
MyroslavShendrik <citrus.user2204@gmail.com>
2025-12-13 05:47:41 +1400

    lesson

--- src/js/add-edit-delete-developer.js
+++ src/js/add-edit-delete-developer.js
@@ -15,21 +15,28 @@ const addDeveloperButton = document.querySelector(".our-developers__button");
 // console.log("addDeveloperButton:",addDeveloperButton);
 const formTitle = document.querySelector(".form-add-edit-developer__title");
 // console.log("formTitle:",formTitle);
+const modalAddEditDeveloper = document.querySelector("[data-modal-add-edit-developer]");
+// console.log("modalAddEditDeveloper:",modalAddEditDeveloper);
 //! 2.2 Змінні для поточної роботи
 //! 3 надати слухачів подій
 addDeveloperButton.addEventListener("click", addDeveloper);
 //! 4 блок функцій 
 function addDeveloper(){
 console.log("додаємо нового розробника")
-
 //! 4.1.1 заміна контенту заголовка модального вікна
+formTitle.textContent = "Додати розробника"
 //! 4.1.2 відкриття модального вікна з формою для збирання даних
+toggleModalAddEditDeveloper()
 //! 4.1.3 зібрати дані з форми по кнопці "submit" і записати ці дані в об'єкт 
 //! 4.1.4 стягнути найсвіжіші дані з LocalStorage в масив об'єктів всіх користувачів
 //! 4.1.5 додати цей об'єкт в масів об'єктів всіх користувачів
 //! 4.1.6 закрити модальне вікно з формою для збирання даних 
 //! 4.1.7 зберегти змінені дані в LocalStorage
-
 };
 
-
+//! відкривання/закривання модального вікна для додавання/редагування розробника
+function toggleModalAddEditDeveloper(){
+    console.log("відкривання/закривання модального вікна для додавання/редагування розробника");
+    modalAddEditDeveloper.classList.toggle("is-hidden");
+    document.body.classList.toggle("no-scroll");
+}
