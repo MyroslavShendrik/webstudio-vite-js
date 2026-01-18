4592f4e34d448b75de47313fad9e3f5fafb53ae2
MyroslavShendrik <citrus.user2204@gmail.com>
2026-01-18 02:18:27 +0200

    lazyload

--- src/js/our-developers-images-intersection-observer.js
+++ src/js/our-developers-images-intersection-observer.js
@@ -2,21 +2,25 @@ const images = document.querySelectorAll('.our-developers-list__img');
 
 const loadImage = (img) => {
     img.src = img.dataset.src;
+
+    const picture = img.parentElement;
+    const sources = picture.querySelectorAll('source');
+
+    sources.forEach(source => {
+        source.srcset = source.dataset.srcset;
+    });
 };
 
-const observerImg = new IntersectionObserver(
-    entries => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                loadImage(entry.target);
-                observerImg.unobserve(entry.target);
-                console.log(`Елемент ${entry.target.alt} видно!`);
-            }
-        });
-    },
-    {
-        threshold: 0.9,
-    }
-);
+const observerImg = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            loadImage(entry.target);
+            observerImg.unobserve(entry.target);
+            console.log(`Завантажено: ${entry.target.alt}`);
+        }
+    });
+}, {
+    threshold: 0.5
+});
 
 images.forEach(img => observerImg.observe(img));
