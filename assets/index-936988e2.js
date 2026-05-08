e9b953d96b9efca38f2f4e912fdd21c2b05098ff
MyroslavShendrik <citrus.user2204@gmail.com>
2026-05-08 11:16:46 +0000

    Deploying to gh-pages from @ MyroslavShendrik/webstudio-vite-js@406faf064a96aa8fd0f50d2468e3e9f37a9f8dd3 🚀

--- assets/index-936988e2.js
+++ assets/index-936988e2.js
@@ -5,7 +5,7 @@ import{H as d}from"./social-list-a2172a6a.js";const g=[{title:"УВАГА ДО �
 <li class="our-developers-list__item">
   <picture>
     <source
-      data-srcset="
+      srcset="
         {{#each images.desktop}}
           {{this}}
         {{/each}}
@@ -14,7 +14,7 @@ import{H as d}from"./social-list-a2172a6a.js";const g=[{title:"УВАГА ДО �
     />
 
     <source
-      data-srcset="
+      srcset="
         {{#each images.tablet}}
           {{this}}
         {{/each}}
@@ -23,7 +23,7 @@ import{H as d}from"./social-list-a2172a6a.js";const g=[{title:"УВАГА ДО �
     />
 
     <source
-      data-srcset="
+      srcset="
         {{#each images.mobile}}
           {{this}}
         {{/each}}
@@ -65,13 +65,13 @@ import{H as d}from"./social-list-a2172a6a.js";const g=[{title:"УВАГА ДО �
 const S="http://localhost:3000/",D="developers",p=`${S}${D}`;console.log("url:",p);const k=document.querySelector(".our-developers-list");async function E(){try{const t=await(await fetch(p)).json();console.log("developers:",t),j(t)}catch(e){console.log("error:",e)}}//! генеруємо HTML за допомогою Handlebars
 const q=d.compile(L);function j(e){const t=e.map(s=>q(s)).join("");console.log("markup:",t),k.innerHTML=t}E();console.log('%c Додаємо/Редагуємо/Видаляємо розробників в секції "Наша команда"',"color: white; background-color: #D33F49");//! 1️⃣ Дані
 let o=JSON.parse(localStorage.getItem("dataDevelopers"))||[],r="";//! 2️⃣ Пошук DOM
-const x=document.querySelector(".our-developers-list"),M=document.querySelector(".our-developers__button"),v=document.querySelector(".form-add-edit-developer__title"),I=document.querySelector("[data-modal-add-edit-developer]"),u=document.querySelector(".modal-add-edit-developer__button-delete"),$=document.querySelector(".form-add-edit-developer"),B=document.getElementById("button-cancel"),l=document.querySelector(".form-add-edit-developer__button-submit"),c=document.querySelector(".form-add-edit-developer__img"),a=document.querySelector(".form-add-edit-developer__input-developer-name"),i=document.querySelector(".form-add-edit-developer__input-developer-position"),f=document.querySelector("[data-modal-confirm-delete]"),C=document.getElementById("confirm-delete-yes"),N=document.getElementById("confirm-delete-no"),R=document.getElementById("confirm-dev-name");//! 3️⃣ Слухачі подій
+const x=document.querySelector(".our-developers-list"),M=document.querySelector(".our-developers__button"),v=document.querySelector(".form-add-edit-developer__title"),I=document.querySelector("[data-modal-add-edit-developer]"),u=document.querySelector(".modal-add-edit-developer__button-delete"),$=document.querySelector(".form-add-edit-developer"),B=document.getElementById("button-cancel"),l=document.querySelector(".form-add-edit-developer__button-submit"),c=document.querySelector(".form-add-edit-developer__img"),i=document.querySelector(".form-add-edit-developer__input-developer-name"),a=document.querySelector(".form-add-edit-developer__input-developer-position"),f=document.querySelector("[data-modal-confirm-delete]"),C=document.getElementById("confirm-delete-yes"),N=document.getElementById("confirm-delete-no"),R=document.getElementById("confirm-dev-name");//! 3️⃣ Слухачі подій
 M.addEventListener("click",U);$.addEventListener("submit",A);B.addEventListener("click",H);x.addEventListener("click",function(e){e.target.classList.contains("our-developers-list__img")&&O(e)});u.addEventListener("click",J);N.addEventListener("click",P);C.addEventListener("click",T);//! 4️⃣ Функції
 //! ДОДАТИ
-function U(){v.textContent="Додати розробника",l.textContent="Додати",u.style.display="none",a.value="",i.value="",n()}//! РЕДАГУВАННЯ
-function O(e){v.textContent="Редагування розробника",l.textContent="Редагувати",u.style.display="block",o=JSON.parse(localStorage.getItem("dataDevelopers"));const t=o.find(s=>s.name===e.target.alt);r=t.name,a.value=t.name,i.value=t.position,c.src=t.images.default,n()}//! ВИДАЛЕННЯ
+function U(){v.textContent="Додати розробника",l.textContent="Додати",u.style.display="none",i.value="",a.value="",n()}//! РЕДАГУВАННЯ
+function O(e){v.textContent="Редагування розробника",l.textContent="Редагувати",u.style.display="block",o=JSON.parse(localStorage.getItem("dataDevelopers"));const t=o.find(s=>s.name===e.target.alt);r=t.name,i.value=t.name,a.value=t.position,c.src=t.images.default,n()}//! ВИДАЛЕННЯ
 function T(){o=JSON.parse(localStorage.getItem("dataDevelopers")),o=o.filter(e=>e.name!==r),localStorage.setItem("dataDevelopers",JSON.stringify(o,null,2)),n(),location.reload()}//! SUBMIT ФОРМИ
-function A(e){if(e.preventDefault(),o=JSON.parse(localStorage.getItem("dataDevelopers")),l.textContent==="Додати"){const t={name:a.value,position:i.value,images:{default:c.src||"./images/developers/default.jpg"},date:new Date().toLocaleString()};o.push(t)}if(l.textContent==="Редагувати"){const t=o.find(s=>s.name===r);t.name=a.value,t.position=i.value;//! ⭐ ФІКС КАРТИНКИ
+function A(e){if(e.preventDefault(),o=JSON.parse(localStorage.getItem("dataDevelopers")),l.textContent==="Додати"){const t={name:i.value,position:a.value,images:{default:c.src||"./images/developers/default.jpg"},date:new Date().toLocaleString()};o.push(t)}if(l.textContent==="Редагувати"){const t=o.find(s=>s.name===r);t.name=i.value,t.position=a.value;//! ⭐ ФІКС КАРТИНКИ
 t.images.default=c.src}localStorage.setItem("dataDevelopers",JSON.stringify(o,null,2)),n()}//! MODAL
 function n(){I.classList.toggle("is-hidden"),document.body.classList.toggle("no-scroll")}function H(){n()}//! CONFIRM DELETE
 function J(e){e.preventDefault(),R.textContent=r,f.classList.remove("is-hidden"),document.body.classList.add("no-scroll")}function P(){f.classList.add("is-hidden"),document.body.classList.remove("no-scroll")}const F=document.querySelectorAll(".our-developers-list__img"),W=e=>{e.src=e.dataset.src,e.parentElement.querySelectorAll("source").forEach(m=>{m.srcset=m.dataset.srcset})},h=new IntersectionObserver(e=>{e.forEach(t=>{t.isIntersecting&&(W(t.target),h.unobserve(t.target),console.log(`Завантажено: ${t.target.alt}`))})},{threshold:.5});F.forEach(e=>h.observe(e));const Y=[{name:"Студія",href:"./index.html",active:!0},{name:"Портфоліо",href:"./portfolio.html",active:!1},{name:"Контакти",href:"#contacts",active:!1}],z={links:Y},G=`{{#each links}}
