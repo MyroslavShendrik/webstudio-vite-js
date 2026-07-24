ce3164cfb2bffbeed08cdd8574d809a4c8369c35
MyroslavShendrik <citrus.user2204@gmail.com>
2026-07-24 10:30:19 +0300

    Update social-list.js

--- src/js/social-list.js
+++ src/js/social-list.js
@@ -1,4 +1,3 @@
-
 const socialLinks = [
   { id: "instagram", url: "https://instagram.com" },
   { id: "twitter", url: "https://twitter.com" },
@@ -6,24 +5,28 @@ const socialLinks = [
   { id: "linkedin", url: "https://linkedin.com" },
 ];
 
-
-const spritePath = new URL("../images/symboldefs.svg", import.meta.url).href;
+const spritePath = "/images/symboldefs.svg";
 
 const followList = document.querySelector(".follow-list");
 
-// Генеруємо й вставляємо <li> елементи
 followList.insertAdjacentHTML(
   "beforeend",
   socialLinks
     .map(
-      (item) => `
-      <li class="follow-list__item">
-        <a class="follow-list__link" href="${item.url}" target="_blank" rel="noopener noreferrer">
-          <svg width="20" height="20">
-            <use href="${spritePath}#${item.id}"></use>
-          </svg>
-        </a>
-      </li>`
+      ({ id, url }) => `
+        <li class="follow-list__item">
+          <a
+            class="follow-list__link"
+            href="${url}"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <svg width="20" height="20">
+              <use href="${spritePath}#${id}"></use>
+            </svg>
+          </a>
+        </li>
+      `
     )
     .join("")
 );
